d9c283c5b5857b3a9381698dac94f6628ea49463
EddieCarrion.github.io <ecar25L@gmail.com>
2023-05-07 19:07:07 -0700

    progress

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -4,6 +4,7 @@ import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+  
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
